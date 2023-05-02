8660cdd90e746dffe39ca4509932880943324588
maxestwell <maxwellpcorcoran@gmail.com>
2023-05-03 01:00:16 +1000

    morestuff

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let Cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+
+  // console.log(x,y)
+  Cursor.style.left = `${x}px`;
+  Cursor.style.top = `${y}px`;
+}
+
+let links = Array.from(document.querySelectorAll("a"));
+
+console.log(links);
+
+links.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    Cursor.classList.add("grow");
+  });
+  link.addEventListener("mouseleave", () => {
+    Cursor.classList.remove("grow");
+  });
+});
