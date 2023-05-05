b2ee9a7ccf56c9d49ec00ee006962e72ff4858b2
maxestwell <maxwellpcorcoran@gmail.com>
2023-05-05 15:40:47 +1000

    ayo

--- js/app.js
+++ js/app.js
@@ -34,20 +34,37 @@ links.forEach((link) => {
   });
 });
 
-// carousel for analog
-var slideIndex = 0;
-carousel();
+// slideshow
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+/* Find all slideshow containers */
+var slideshowContainers = document.getElementsByClassName(
+  "slideshow-container"
+);
+/* For each container get starting variables */
+for (let s = 0; s < slideshowContainers.length; s++) {
+  /* Read the new data attribute */
+  var cycle = slideshowContainers[s].dataset.cycle;
+  /* Find all the child nodes with class mySlides */
+  var slides = slideshowContainers[s].querySelectorAll(".mySlides");
+  var slideIndex = 0;
+  /* Now we can cycle slides, but this recursive function must have parameters */
+  /* slides and cycle never change, those are unique for each slide container */
+  /* slideIndex will increase during each iteration */
+  showSlides(slides, slideIndex, cycle);
+}
+
+/* Function is alsmost same, but now it uses 3 new parameters */
+function showSlides(slides, slideIndex, cycle) {
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > x.length) {
+  if (slideIndex > slides.length) {
     slideIndex = 1;
   }
-  x[slideIndex - 1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  slides[slideIndex - 1].style.display = "block";
+  /* Calling same function, but with new parameters and cycle time */
+  setTimeout(function () {
+    showSlides(slides, slideIndex, cycle);
+  }, cycle);
 }
