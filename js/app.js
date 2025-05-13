fa0c0456348f84db990b4e152356c5e240938b1f
maxestwell <maxwellpcorcoran@gmail.com>
2025-05-13 20:06:19 +1000

    grid

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function hideAllContentDivs() {
+function hideContentDivs() {
   const contentDivs = document.getElementsByClassName("content");
   for (let i = 0; i < contentDivs.length; ++i) {
     const div = contentDivs[i];
@@ -6,40 +6,19 @@ function hideAllContentDivs() {
   }
 }
 
-const gridMax = document.querySelector(".grid-max");
+const gridMaxElements = document.querySelectorAll(".grid-max");
 
-if (gridMax) {
+gridMaxElements.forEach((gridMax) => {
   const computedStyle = window.getComputedStyle(gridMax);
   const columns = computedStyle
     .getPropertyValue("grid-template-columns")
     .split(" ").length;
-  const rows = computedStyle
-    .getPropertyValue("grid-template-rows")
-    .split(" ").length;
 
-  const totalCells = columns * rows;
+  const totalCells = columns;
 
   for (let i = 0; i < totalCells; i++) {
     const cell = document.createElement("span");
     cell.classList.add("grid-cell");
     gridMax.appendChild(cell);
   }
-
-  // const m = [
-  //   2, 101, 201, 301, 401, 501, 601, 701, 103, 204, 105, 6, 107, 207, 307, 407,
-  //   507, 607, 707,
-  // ];
-  // const a = [
-  //   12, 111, 210, 310, 410, 510, 610, 710, 113, 214, 314, 414, 514, 614, 714,
-  // ];
-
-  // const targetIndices = [...m, ...a];
-
-  // targetIndices.forEach((index) => {
-  //   const specificCell = gridMax.children[index];
-  //   if (specificCell) {
-  //     specificCell.style.backgroundColor = "#ffc033";
-  //     specificCell.style.pointerEvents = "none";
-  //   }
-  // });
-}
+});
